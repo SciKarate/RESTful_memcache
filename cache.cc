0785fb70bedf11dee44e1f8f41422f12c22486de
Josh <dash219@gmail.com>
2018-10-21 16:12:20 -0700

    1-2 100% done;  3-5 maybe done

--- cache.cc
+++ cache.cc
@@ -1,7 +1,7 @@
 #include <cache.hh>
 #include <unordered_map>
 #include <cstring> //for "std::memcpy" in set
-#include <iostream>
+#include <iostream> //for "evicting key..." printout
 
 struct Cache::Impl {
 private:
@@ -17,27 +17,28 @@ public:
 	{
 	}
 
-	~Impl() {for (auto kvpair : data_) {del(kvpair.first);}}
+	~Impl() {for (auto kvpair : data_) {del(kvpair.first);}} //free all ptrs
 
 	void set(key_type key, val_type val, index_type size)
 	{
-		if(data_[key] != 0) {free(data_[key]);}
-		void *val_ptr = malloc(size);
-		std::memcpy(val_ptr, val, size);
-		data_[key] = val_ptr;
-		memused_ += sizeof(data_[key]); //somehow increase memused
-		if(memused_ > maxmem_)
+		if(data_[key] != 0) {free(data_[key]);} //free existing ptr if editing
+		void *val_ptr = malloc(size);			//malloc an empty pointer...
+		std::memcpy(val_ptr, val, size);		//and deep-copy to it.
+		data_[key] = val_ptr;					//then store it in data_, and...
+		memused_ += sizeof(data_[key]); 		//increase memused_ by its size
+
+		if(memused_ > maxmem_)	// if we need to do some eviction...
 		{
 			std::cout << "evicting key...\t\t" << (data_.begin()->first) << std::endl;
-			del(data_.begin()->first);
+			del(data_.begin()->first); //delete the first key in data_
 		}
-		//set key, value pair with key and val
 	}
 	
 	val_type get(key_type key, index_type& val_size)
 	{
-		if(data_[key] != 0) {return data_[key];}
-		else {data_.erase(key); return NULL;}
+		if(data_[key] != 0) {return data_[key];}//fetch key if exists.
+		else {data_.erase(key); return NULL;} 	//if key is nonexistent,
+												//make sure we don't keep it!
 		//takes key and size of retrieved value
 		//return a pointer to key in array
 	}
@@ -46,9 +47,9 @@ public:
 	{
 		if(data_[key] != 0)
 		{
-			memused_ -= sizeof(data_[key]);
-			free(data_[key]);
-			data_.erase(key);
+			memused_ -= sizeof(data_[key]);	//decrement memuse
+			free(data_[key]);				//free pointer to data
+			data_.erase(key);				//make data_ forget the key.
 		}
 	}
 
--- cache_tester.cc
+++ cache_tester.cc
@@ -36,8 +36,9 @@ Cache::index_type my_hash_func(Cache::key_type key)
 
 int main()
 {
-	Cache test_cache(22, [](){return 0;}, my_hash_func);
+	Cache test_cache(32, [](){return 0;}, my_hash_func); //create a cache
 
+	//create some test variables to store
 	char char_test[10] = "abcdefghi";
 	std::string string_test = "blorp!!";
 	std::string* string_test_ptr = &string_test;
@@ -47,14 +48,20 @@ int main()
 	int int_test2 = 172;
 	int* int_test_ptr2 = &int_test2;
 
+	//store all those test variables
 	test_cache.set("int_keya", static_cast<Cache::val_type>(int_test_ptr), sizeof(int_test));
 	test_cache.set("my_keyb", static_cast<Cache::val_type>(char_test), sizeof(char_test));
 	test_cache.set("str_keyc", static_cast<Cache::val_type>(string_test_ptr), sizeof(string_test));
 	test_cache.set("intarr_keyd", static_cast<Cache::val_type>(intarr_test), sizeof(intarr_test));
 
 	using namespace std;
-	uint32_t sz;
+	uint32_t sz; //used to store size for get calls
 
+	//fun with ints! we...
+	//1) get our stored value
+	//2) try to pull out a value that is not stored
+	//3) modify a stored value
+	//4) get our new, modified value out.
 	sz = sizeof(int);
 	cout << "Stored 194:\t\t" << intcast(test_cache.get("int_keya", sz)) << endl;
 	cout << "Unstored int:\t\t" << intcast(test_cache.get("int_key", sz)) << endl;
@@ -65,7 +72,7 @@ int main()
 	cout << "Stored abcdefghi:\t" << chararrcast(test_cache.get("my_keyb", sz)) << endl;
 	
 	sz = sizeof(string);
-	cout << "Stored blorp!!:\t\t" << strcast(test_cache.get("str_keyc", sz)) << endl;
+	cout << "Evicted blorp!!:\t" << strcast(test_cache.get("str_keyc", sz)) << endl;
 
 	test_cache.del("int_keya");
 	test_cache.del("my_keyb");
@@ -78,7 +85,7 @@ int main()
 	cout << "Deleted chararr:\t" << chararrcast(test_cache.get("my_keyb", sz)) << endl;
 
 	sz = sizeof(string);
-	cout << "Deleted str:\t\t" << strcast(test_cache.get("str_keyc", sz)) << endl;
+	cout << "Deleted, evicted str:\t" << strcast(test_cache.get("str_keyc", sz)) << endl;
 	sz = sizeof(string);
 	test_cache.set("str_keyc", static_cast<Cache::val_type>(string_test_ptr), sizeof(string_test));
 	cout << "Stored blorp!!:\t\t" << strcast(test_cache.get("str_keyc", sz)) << endl;
