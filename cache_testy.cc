48360dab8410b20b1db0e8bc9074368414fc01ab
Josh <dash219@gmail.com>
2018-10-30 13:39:08 -0700

    fixed include implementation

--- cache_testy.cc
+++ cache_testy.cc
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch.hpp>
-#include <cache.hh>
+#include "catch.hpp"
+#include "cache.hh"
 #include <iostream>
 #include <functional>
 //build with "g++ cache.cc cache_tester.cc"
