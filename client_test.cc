f041d0be784f487e4a26bad35c0becd0ed86c1fd
Josh <dash219@gmail.com>
2018-11-12 11:29:24 -0800

    working on client

--- client_test.cc
+++ client_test.cc
@@ -1,33 +1,37 @@
-#include <curl/curl.h>
-#include <string.h>
-#include <stdio.h>
-//g++ -lcurl
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
 
-int main()
+
+using namespace curlpp::options;
+
+int main(int, char **)
 {
-	CURL *curl;
-  	CURLcode res;
- 
-  static const char *postthis = "moo mooo moo moo";
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "localhost:18080/add_json");
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postthis);
- 
-    /* if we don't provide POSTFIELDSIZE, libcurl will strlen() by
-       itself */ 
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, (long)strlen(postthis));
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
-  }
+	try
+	{
+		// That's all that is needed to do cleanup of used resources (RAII style).
+		curlpp::Cleanup myCleanup;
+
+		// Our request to be sent.
+		curlpp::Easy myRequest;
+
+		// Set the URL.
+		myRequest.setOpt<Url>("http://example.com");
+
+		// Send request and get a result.
+		// By default the result goes to standard output.
+		myRequest.perform();
+	}
+
+	catch(curlpp::RuntimeError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	catch(curlpp::LogicError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+    
   return 0;
 }
