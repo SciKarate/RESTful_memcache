517255fbc23d3e7aebf46cf6c8b0698f9ccb5a13
Josh <dash219@gmail.com>
2018-11-12 09:56:16 -0800

    figured out basic html

--- crow_test.cc
+++ crow_test.cc
@@ -1,10 +1,54 @@
-//g++ crow_test.cc -lboost_system -pthread
+//g++ crow_test.cc cache.cc -lboost_system -pthread
 
 #include "crow.h"
+#include "cache.hh"
+
+int cache_size = 64;
+Cache server_cache(cache_size);
 
 int main()
 {
     crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/memsize")
+	.methods("GET"_method)
+	([](const crow::request& req){
+		std::ostringstream os;
+    	os << server_cache.space_used();
+	    os << std::endl;
+		return crow::response{os.str()};
+	});
+
+	CROW_ROUTE(app, "/key/<str>")
+	.methods("HEAD"_method, "GET"_method, "DELETE"_method)
+	([](const crow::request& req, std::string k){
+    	if (req.method == "HEAD"_method)
+    	{
+    		std::ostringstream os;
+    		os << "hello!";
+	    	os << std::endl;
+			return crow::response{os.str()};
+    	}
+    	else if (req.method == "GET"_method)
+    	{
+    		std::ostringstream os;
+    		os << k;
+	    	os << std::endl;
+			return crow::response{os.str()};
+    	}
+    	else if (req.method == "DELETE"_method)
+    	{
+    		std::ostringstream os;
+    		os << "howdy";
+    		os << k;
+	    	os << std::endl;
+			return crow::response{os.str()};
+    	}
+    	else
+    	{
+    		return crow::response(404);
+    	}
+	});
 
     CROW_ROUTE(app, "/json")
 	([]{
@@ -15,27 +59,16 @@ int main()
 
 	//curl -H "Content-Type: application/json" -X POST -d @user.json localhost:18080/add_json
     CROW_ROUTE(app, "/add_json")
-	.methods("POST"_method, "GET"_method)
+	.methods("POST"_method)
 	([](const crow::request& req){
-		if(req.method == "GET"_method)
-		{
-			if ((req.url_params.get("v") != nullptr) & (req.url_params.get("q") != nullptr))
-            {
-               	char x = req.url_params.get("v");
-            }
-            return response(200, "You used GET");
-		}
-		else if (req.method == "POST_method")
-		{
-	    	auto x = crow::json::load(req.body);
-	    	if (!x)
-	    	    return crow::response(400);
-		    int sum = x["a"].i()+x["b"].i();
-		    std::ostringstream os;
-	    	os << sum;
-		    os << std::endl;
-		    return crow::response{os.str()};
-		}
+		auto x = crow::json::load(req.body);
+	    if (!x)
+	        return crow::response(400);
+		int sum = x["a"].i()+x["b"].i();
+		std::ostringstream os;
+    	os << sum;
+	    os << std::endl;
+		return crow::response{os.str()};
 	});
 
 	//curl localhost:18080/hello/30
@@ -48,5 +81,5 @@ int main()
     	return crow::response(os.str());
 	});
 
-    app.port(18080).run();
+    app.port(18085).run();
 }
