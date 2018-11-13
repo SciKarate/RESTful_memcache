0b9bd86df1f550f7abb70ef306757cc7cdf0b55e
Josh <dash219@gmail.com>
2018-11-13 13:36:04 -0800

    have all requests figured out.

--- testing_client.cpp
+++ testing_client.cpp
@@ -191,7 +191,12 @@ int main()
   //below is delete code
   if(curl)
   {
-    
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    curl_easy_cleanup(curl); 
   }
   curl_global_cleanup();
 }
